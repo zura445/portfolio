ca692a8403d39ab369d20d9d213d573def4c8d23
zura tetrashvili <zuratetra@gmail.com>
2024-07-17 16:19:16 +0400

    finish about me file

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,30 +1,63 @@
+import { useState } from "react";
+import jobsData from "../../src/jobsData.json";
+
 function AboutMe() {
+  const { jobs } = jobsData;
+  const [showAll, setShowAll] = useState(false);
+
+  const visibleJobs = showAll ? jobs : jobs.slice(0, 2);
+
+  const toggleJobsVisibility = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div>
-      <div className="flex justify-center text-4xl mt-8">
+      <div className="flex justify-center text-4xl mt-8 text-gray-500">
         {"{ "}/{" }"}
       </div>
-      <div className="flex justify-center text-8xl mt-8">
+      <div className="flex justify-center text-8xl mt-8 text-gray-500">
         {"<"}About_me{">"}
       </div>
-      <p className="m-auto pt-8 max-w-[800px]">
+      <p className="m-auto pt-8 max-w-[800px] text-gray-400">
         ...Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
         explicabo, repellat maxime, debitis quaerat impedit velit deleniti
         minima rerum quis ducimus sint tempore tenetur animi harum sed? Dolore,
         porro delectus.
       </p>
-      <div className="grid grid-cols-2 px-10 mt-8">
-        <div className="flex items-center">
-          <img
-            src="/images/tnet-logo.png"
-            alt="tnet-logo"
-            className="rounded-full w-10 h-10"
-          />
-          <p className="ml-4">Front-End Developer at Vendoo.ge</p>
-        </div>
-        <p>gexdghjmkl</p>
-        <p>htghbjnmk,lj</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-10 mt-8">
+        {visibleJobs.map((job, index) => (
+          <div key={index} className="flex flex-col">
+            <div className="flex items-center mb-4">
+              <img
+                src={job.logo}
+                alt={`${job.company}-logo`}
+                className="rounded-full w-10 h-10"
+              />
+              <div className="ml-4 text-gray-400">
+                <p>{job.title}</p>
+                <p>{job.company}</p>
+                <p>{job.duration}</p>
+              </div>
+            </div>
+            <ul className="text-gray-400 list-disc pl-5">
+              {job.responsibilities.map((responsibility, respIndex) => (
+                <li key={respIndex}>{responsibility}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
+      {jobs.length > 2 && (
+        <div className="flex justify-center mt-8">
+          <button
+            onClick={toggleJobsVisibility}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+          >
+            {showAll ? "See less" : "See more"}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
