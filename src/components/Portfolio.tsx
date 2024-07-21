627f38cd7aeb2154494c7591422cdc7c532b065f
zura tetrashvili <zuratetra@gmail.com>
2024-07-21 19:30:57 +0400

    add two link

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -9,6 +9,36 @@ function Portfolio() {
           laboriosam laudantium atque recusandae distinctio, est et quidem
         </p>
       </div>
+      <div className="font-mono grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 pb-10">
+        <div className="w-11/12 h-[400px]">
+          <a
+            href="https://gba-project.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/gba.jpg"
+              alt="GBA image"
+              className="w-full h-full object-contain"
+            />
+            <p className="mt-4">Georgian Bartenders Association</p>
+          </a>
+        </div>
+        <div className="w-11/12 h-[400px]">
+          <a
+            href="https://zuratours.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/tour.jpg"
+              alt="GBA image"
+              className="w-full h-full object-contain"
+            />
+            <p className="mt-4">Small travel company</p>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
