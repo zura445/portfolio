73f71e07b30b7d8b8b0b7cd556e826de057d133d
zura tetrashvili <zuratetra@gmail.com>
2024-07-22 18:30:11 +0400

    add my projects

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,44 +1,35 @@
+import portfolioData from "../../src/portfolioData.json";
+
 function Portfolio() {
   return (
     <div className="mt-8">
-      <div className="flex flex-col justify-center items-center">
-        <p className=" text-3xl">( ! )</p>
-        <p className="text-4xl mt-4">{"<Portfolio>"}</p>
-        <p className="max-w-[700px] text-center mt-2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
-          laboriosam laudantium atque recusandae distinctio, est et quidem
-        </p>
-      </div>
-      <div className="font-mono grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 pb-10">
-        <div className="w-11/12 h-[400px]">
-          <a
-            href="https://gba-project.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/gba.jpg"
-              alt="GBA image"
-              className="w-full h-full object-contain"
-            />
-            <p className="mt-4">Georgian Bartenders Association</p>
-          </a>
+      {portfolioData.map((portfolio, index) => (
+        <div key={index} className="">
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-3xl">( ! )</p>
+            <p className="text-4xl mt-4">{"<Portfolio>"}</p>
+            <p className="max-w-[700px] text-center mt-2">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              laboriosam laudantium atque recusandae distinctio, est et quidem
+            </p>
+          </div>
+          <div className="font-mono grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 pb-10">
+            {portfolio.projects.map((project, projectIndex) => (
+              <div key={projectIndex} className="w-[95%] border">
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={project.image}
+                    alt={`${project.name} image`}
+                    style={{ height: project.height }}
+                    className="w-full object-contain"
+                  />
+                  <p className="mt-4">{project.name}</p>
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="w-11/12 h-[400px]">
-          <a
-            href="https://zuratours.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/tour.jpg"
-              alt="GBA image"
-              className="w-full h-full object-contain"
-            />
-            <p className="mt-4">Small travel company</p>
-          </a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
