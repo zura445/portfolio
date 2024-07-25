fb4d147371af9c61c5fb69d87202b71e3f40de49
zura tetrashvili <zuratetra@gmail.com>
2024-07-25 21:59:47 +0400

    bug fixing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <div className="flex">
-      <div className=" text-xl flex flex-col justify-center">
+      <div className=" text-xl flex flex-col justify-center ml-4">
         <p className="text-gray-400">Zura</p>
         <p className="text-gray-500">// Web_dew</p>
       </div>
