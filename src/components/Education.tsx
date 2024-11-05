4601845c5ddda730fd6fb47e2671d64a62715ce9
zura tetrashvili <zuratetra@gmail.com>
2024-11-05 19:32:30 +0400

    add my experience

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,5 +1,15 @@
 function Education() {
-  return <div>Education</div>;
+  return (
+    <div className="text-center flex flex-col items-center justify-center">
+      <div className="text-xl mt-6">( $ )</div>
+      <div className="text-4xl mt-4">Education</div>
+      <p className="max-w-[700px] mt-4 font-mono">
+        I began my studies at "AlgoUni" School in April 2024 and I am currently
+        still enrolled. During this time, I have successfully completed courses
+        in JavaScript, React, and Next.js.
+      </p>
+    </div>
+  );
 }
 
 export default Education;
